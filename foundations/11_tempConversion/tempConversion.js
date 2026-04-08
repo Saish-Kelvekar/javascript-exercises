5ea3b7f84a06f6bac56f274575b67122f79188ec
Saish-Kelvekar <kelvekarsaish@gmail.com>
2026-04-08 19:31:15 +0530

    completed the temperature conversion question

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahernitTemp) {
+  if(fahernitTemp>=0){}
+  var celsius=(fahernitTemp-32)*5/9;
+  var roundedCelsius=Math.round(celsius*10)/10;
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  var fahrenheit=(celsiusTemp*(9/5))+32;
+  var roundedFahrenheit=Math.round(fahrenheit*10)/10;
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
