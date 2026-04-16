8d8f02a2e41050477b7d2c4c2d3ea5c6ebed065c
Saish-Kelvekar <kelvekarsaish@gmail.com>
2026-04-16 23:47:51 +0530

    Completed the fibonacci problem     came to know about the logic about the fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let a=1;
+    let b=1;
+    let i=1;
+    if(n<0){
+        return "OOPS";
+    }
+    else if(n==0){
+        return 0;
+    }
+    else{
+    while(i<=n-2){
+        let next=a+b;
+        a=b;
+        b=next;
+        i++;
+    }
+    return b;}
 };
 
 // Do not edit below this line
