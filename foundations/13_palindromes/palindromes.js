87d917bdd97a907c39826ab27a5fb8397d61f2ab
Saish-Kelvekar <kelvekarsaish@gmail.com>
2026-04-16 23:00:51 +0530

    Completed the palindrome question     got to know about the palindrome logic

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+newStr=str.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
 
+
+let i=0;
+let j=newStr.length-1;
+while(i<=j){
+    if(newStr[i]!=newStr[j]){
+        return false;
+    }
+    i++;
+    j--;
+}
+return true;
 };
 
 // Do not edit below this line
