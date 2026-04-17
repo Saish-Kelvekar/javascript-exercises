1fb4447ca973ec31a244eb5bbd65bf6e8fbdefa7
Saish-Kelvekar <kelvekarsaish@gmail.com>
2026-04-17 15:31:25 +0530

    Completed the find the Oldest death problem     Solved using the forEach method     Got to know how to use the Date()function

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(obj=>{
+        let birth=obj.yearOfBirth;
+        let death=obj.yearOfDeath;
+        if(death===undefined){
+            death=new Date().getFullYear();
+        }
+        obj.lived=death-birth;
+    });
+    people.sort((a,b)=>{
+       return b.lived - a.lived ;
+    });
+
+    return people[0];
 
 };
 
